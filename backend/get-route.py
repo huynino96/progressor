129dfdb66daa31eaf129b4db74acf120210af65d
Huy Ho <huynino0496@gmail.com>
2020-07-25 18:02:34 +0700

    Get All User's Repos

---
+++ backend/get-route.py
@@ -0,0 +1,11 @@
+import requests
+from pprint import pprint
+
+# github username
+username = "huynino96"
+# url to request
+url = f"https://api.github.com/users/{huynino96}"
+# make the request and return the json
+user_data = requests.get(url).json()
+# pretty print JSON data
+pprint(user_data)
