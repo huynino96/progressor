cdc64feff91fea78b580182cf48e00388087535d
Huy Ho <tuanhuy0496@yahoo.com>
2020-08-11 15:46:33 +0700

    Add more comments

--- backend/library.py
+++ backend/library.py
@@ -3,34 +3,39 @@ import base64
 import json
 
 
-def constructURL(user = "404",repo_name= "404",path_to_file= "404",url= "404"):
-  url = url.replace("{user}",user)
-  url = url.replace("{repo_name}",repo_name)
-  url = url.replace("{path_to_file}",path_to_file)
-  return url
+# API endpoint
+def constructURL(user="404", repoName="404", pathToFile="404", url="404"):
+    url = url.replace("{user}", user)
+    url = url.replace("{repo_name}", repoName)
+    url = url.replace("{path_to_file}", pathToFile)
+    return url
+
 
 user = 'github'
-repo_name = 'fetch'
-path_to_file = 'package.json'
-json_url ='https://api.github.com/repos/github/fetch/contents/package.json'
+repoName = 'fetch'
+pathToFile = 'package.json'
+jsonUrl = 'https://api.github.com/repos/github/fetch/contents/package.json'
 
-# json_url ='https://api.github.com/repos/{user}/{repo_name}/contents/{path_to_file}'
+ # forms the correct URL
+json_url = constructURL(user, repoName, pathToFile, jsonUrl) 
 
-json_url = constructURL(user,repo_name,path_to_file,json_url) #forms the correct URL
-response = requests.get(json_url) #get data from json file located at specified URL 
+# get data from json file located at specified URL
+response = requests.get(json_url)
+
 
 if response.status_code == requests.codes.ok:
     jsonResponse = response.json()  # the response is a JSON
-    #the JSON is encoded in base 64, hence decode it
+    # the JSON is encoded in base 64, hence decode it
     content = base64.b64decode(jsonResponse['content'])
-    #convert the byte stream to string
+    # convert the byte stream to string
     jsonString = content.decode('utf-8')
     finalJson = json.loads(jsonString)
 else:
     print('Content was not found.')
 
+# Use only the DevDependencies
 dependencies = finalJson["devDependencies"]
 
+# The lenght is the number of the libraries that have been added 
 print(len(dependencies))
-# for key, value in finalJson.items():
-#     print("The key and value are ({}) = ({})".format(key, value))
+
