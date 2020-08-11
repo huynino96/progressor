a0e2ccf87dc16cac400c2df1c1d6998e9db36340
Huy Ho <tuanhuy0496@yahoo.com>
2020-08-11 18:08:10 +0700

    Add LOC to List

--- backend/library.py
+++ backend/library.py
@@ -16,22 +16,25 @@ repoName = 'fetch'
 pathToFile = 'package.json'
 jsonUrl = 'https://api.github.com/repos/github/fetch/contents/package.json'
 
- # forms the correct URL
+# forms the correct URL
 json_url = constructURL(user, repoName, pathToFile, jsonUrl) 
 
 # get data from json file located at specified URL
 response = requests.get(json_url)
 
 
+
+
 if response.status_code == requests.codes.ok:
     jsonResponse = response.json()  # the response is a JSON
     # the JSON is encoded in base 64, hence decode it
+    print(jsonResponse)
     content = base64.b64decode(jsonResponse['content'])
     # convert the byte stream to string
     jsonString = content.decode('utf-8')
     finalJson = json.loads(jsonString)
 else:
-    print('Content was not found.')
+    print('Content not found.')
 
 # Use only the DevDependencies
 dependencies = finalJson["devDependencies"]
---
+++ backend/loc.py
@@ -0,0 +1,22 @@
+import json
+import requests
+
+# Getting data from GitHub API
+response = requests.get("https://api.github.com/repos/andris9/fetch/contents")
+#print(response)
+users = json.loads(response.text)
+
+# print(type(users))
+#print(users)
+textList = []
+for d in users:
+    x = str(d["name"])
+    
+    url = "https://api.github.com/repos/andris9/fetch/contents/{x}"
+    endUrl = url.replace('{x}', x)
+
+    response1 = requests.get(endUrl)
+    allText = json.loads(response1.text)
+    textList.append(allText)
+
+print(textList)
