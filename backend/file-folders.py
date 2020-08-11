09cf7a3c4a1d4a8bec3940c9d83653eb3047cebf
Huy Ho <tuanhuy0496@yahoo.com>
2020-08-11 15:05:43 +0700

    Counting Files and Folders

---
+++ backend/file-folders.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+# Getting data from GitHub API
+response = requests.get("https://api.github.com/repos/andris9/fetch/contents")
+users = json.loads(response.text)
+strDict = str(users)
+
+# Counting number of Files
+def countFile():
+    strFile = "\'file\'"
+    fileCounter = strDict.count(strFile)
+    print("Number of Files: ",fileCounter)
+
+# Counting nuber of Folders
+def countFolder():
+    strFolder = "\'dir\'"
+    folderCounter = strFolder.count(strFolder)
+    print("Number of Folders: ",folderCounter)
+
+countFile()
+countFolder()
+
