f5dff42b9f41d52470acd7f43519f152c5c1fa24
Huy Ho <tuanhuy0496@yahoo.com>
2020-08-19 15:21:30 +0700

    Fixing Bugs when read Dict

--- backend/readDetails.py
+++ backend/readDetails.py
@@ -4,7 +4,38 @@ f = open("data.json", "r")
 file = f.read()
 data = json.loads(file)
 
-print(type(data))
+# print(type(data))
+
+dictOnly = []
+listOnly = []
+for i in data:
+    if isinstance(i, list):
+        listOnly.append(i)
+    else:
+        dictOnly.append(i)
+
+dictOnly1 = []
+for index in listOnly:
+    for i1 in index:
+        dictOnly1.append(i1)
+
+totalDict = dictOnly + dictOnly1
+
+fileOnly = []
+folderOnly = []
+
+for i2 in totalDict:
+    if i2["type"] == 'file':
+        fileOnly.append(i2)
+    elif i2["type"] == 'dir':
+        folderOnly.append(i2)
+
+print(folderOnly)
+
+
+
+
+
 
 
 
