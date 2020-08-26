4e841d05e940b6363efe3a743a4ec755a9ef4d0d
Huy Ho <tuanhuy0496@yahoo.com>
2020-08-26 14:52:10 +0700

    Decoded all content in Master File

--- backend/test.py
+++ backend/test.py
@@ -1,9 +1,22 @@
 import json
 import requests
+import base64
 """Fetch data from local files"""
 f1 = open("masterFile.json", "r")
 file1 = f1.read()
 data1 = json.loads(file1)
 
+content =[]
 for i in data1:
-    print(type(i))
+    j = i["content"]
+    content.append(j)
+
+    
+
+allFileList = []
+for indexContent in content:
+    decodedContent = base64.b64decode(indexContent)
+    f = open("allFile.txt", "a")
+    allFileText = f.write(str(decodedContent))
+    
+
