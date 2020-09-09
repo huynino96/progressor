90b456b4f9131fe094a86c75890f5c8eecfbfb1b
Huy Ho <tuanhuy0496@yahoo.com>
2020-09-09 15:41:35 +0700

    Export csv for method

--- backend/method_count.py
+++ backend/method_count.py
@@ -1,9 +1,13 @@
 import requests
 import base64
 import json
+import constants
+import csv
+import os
+import datetime
 
 """Fetch data from local files"""
-f1 = open("allFile.txt", "r")
+f1 = open(constants.INPUT_FOLDER + "/allFile.txt", "r")
 file1 = f1.read()
 
 substr1 = "function(){"
@@ -16,5 +20,20 @@ funcCount1 = file1.count(substr3)
 arrowFuncCount = file1.count(substr2)
 
 totalFunc = funcCount + arrowFuncCount + funcCount1
-print(totalFunc)
 
+today = datetime.datetime.now()
+
+# File CSV
+filePath = constants.OUTPUT_FOLDER + '/methods.csv'
+fileEmpty = True
+try:
+    fileEmpty = os.stat(filePath).st_size == 0
+except:
+    fileEmpty = True
+
+# The lenght is the number of the method that have been added
+with open(filePath, 'a', newline='') as file:
+    writer = csv.writer(file)
+    if fileEmpty:
+        writer.writerow(["Date", "Number of methods"])
+    writer.writerow([today.strftime('%x %X'), str(totalFunc)])
