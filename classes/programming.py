c8ca53ab396c5cfd03bbfb0c075c16632b41c58d
duykhangtran1210@gmail.com <duykhangtran1210@gmail.com>
2020-11-24 15:08:48 +0700

    create handler and utitlity

---
+++ classes/programming.py
@@ -0,0 +1,15 @@
+class Programming:
+    def count_class(self):
+        pass
+
+    def count_comment(self):
+        pass
+
+    def count_library(self):
+        pass
+
+    def count_method(self):
+        pass
+
+    def count_unit_test(self):
+        pass
---
+++ classes/utility.py
@@ -0,0 +1,12 @@
+class Utility:
+    def count_file_and_folder(self):
+        pass
+
+    def count_line_of_code(self):
+        pass
+
+    def count_api(self):
+        pass
+
+    def count_database(self):
+        pass
