0a716368123b753f7644f07f87f868389e9032ab
T.K.D <duykhangtran1210@gmail.com>
2020-11-24 14:53:11 +0700

    Javascript

---
+++ classes/javascript.py
@@ -0,0 +1,12 @@
+import programming as Programming
+
+
+class Jasvascript(Programming):
+    def count_library(self):
+        pass
+
+    def count_method(self):
+        pass
+
+    def count_unit_test(self):
+        pass
