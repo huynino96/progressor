a7bbc683dee8d06901449c996f2059149b3a0862
T.K.D <duykhangtran1210@gmail.com>
2020-11-24 14:51:56 +0700

    Java update

---
+++ classes/java.py
@@ -0,0 +1,12 @@
+import programming as Programming
+
+
+class Java(Programming):
+    def count_library(self):
+        pass
+
+    def count_method(self):
+        pass
+
+    def count_unit_test(self):
+        pass
